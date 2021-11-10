f56720075004341423f87afa940c42ab8fcf017a
PetrochenkoVA <uzbek150885@gmail.com>
2021-11-10 09:31:32 +0300

    branchJunet4

--- src/test/java/ru/netology/auto/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/auto/CashbackHackServiceTest.java
@@ -1,36 +1,18 @@
 package ru.netology.auto;
 
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
 
 public class CashbackHackServiceTest {
-    @Test
+    @org.junit.Test
     public void remain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 800;
         int actual = service.remain(amount);
         int expected = 200;
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 }
-/*
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CashbackHackServiceTest {
-
-    @Test
-    void remain() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 800;
-        int actual = service.remain(amount);
-        int expected =200;
-
-        assertEquals (expected, actual);
-    }
-}
-*/
