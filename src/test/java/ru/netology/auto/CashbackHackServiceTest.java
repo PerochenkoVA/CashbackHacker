502be62f0097f3fd9a5c4c4922e6546b77150148
Vladimir Petrochenko <83920626+PerochenkoVA@users.noreply.github.com>
2021-12-03 07:44:32 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/auto/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/auto/CashbackHackServiceTest.java
@@ -15,4 +15,13 @@ public class CashbackHackServiceTest {
 
         Assert.assertEquals(expected, actual);
     }
+     @Test
+    void calculateOneThousand(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected =0;
+
+        assertEquals (expected, actual);
+    }
 }
